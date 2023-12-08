10354a9871ada631856c36c9931c403ed745b049
MonikySouzaLopes <monikysouzalopes@gmail.com>
2023-12-08 09:35:13 -0400

    Atualizado 08.12 Parte 1
    Tirei o nome da música como link para acessar a página Detalhe e criei um botão para acessar a página Detalhe para melhor visualização.

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -4,8 +4,9 @@ export default function Card({lista}){
     return(
         lista.map((video)=>
             <div key= {video.id}>
-                <Link to={`/Detalhe/${video.id}`}>
                 <p><strong>Nome da música:</strong>{video.NomeDaMusica}</p>
+                <Link to={`/Detalhe/${video.id}`}>
+                <button>Ver mais</button>
                 </Link>
                 
                <iframe width="300" height="200" src={
